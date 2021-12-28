ded18efc61a2b02cdb9f45cf1f61c3370aa9e449
axlan <feros32@gmail.com>
2021-12-27 17:02:29 -0800

    Incorperated helper into fork.

---
+++ WifiManagerParamHelper.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+
+struct ParamEntry {
+  const char *id;
+  const char *label;
+  int max_len;
+};
+
+class WifiManagerParamHelper {
+public:
+  WifiManagerParamHelper(WiFiManager &wm) : wm_(wm) {}
+
+  void Init(uint16_t preamble, const ParamEntry *entries, size_t entries_len);
+
+  const char *GetSetting(size_t idx) const;
+
+private:
+  static constexpr size_t HEADER_SIZE = sizeof(uint16_t) * 2;
+  size_t _data_size = 0;
+  WiFiManager &wm_;
+  std::vector<WiFiManagerParameter> parameters_;
+
+  void OnParamCallback();
+};
